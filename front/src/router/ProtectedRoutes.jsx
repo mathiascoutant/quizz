deb13db4f4c090916898b8c2c02746cc5c7f1fab
Draxx0 <williamfort.lmgl@gmail.com>
2024-10-17 15:42:28 +0200

    :bug: fix protected route

--- front/src/router/ProtectedRoutes.jsx
+++ front/src/router/ProtectedRoutes.jsx
@@ -1,8 +1,8 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useSessionStore } from '../store/session.store';
 
-export const ProtectedRoute = () => {
+export const ProtectedRoute = ({ children }) => {
   const session = useSessionStore((state) => state.session);
 
-  return session ? <Outlet /> : <Navigate to="/login" />;
+  return session ? children : <Navigate to="/login" />;
 };
