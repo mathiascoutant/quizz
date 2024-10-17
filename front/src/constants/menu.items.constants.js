a11301f0e56347532882e7277f6a23f6466a9132
Draxx0 <williamfort.lmgl@gmail.com>
2024-10-17 12:36:50 +0200

    :building_construction: make some architecturals modifications

---
+++ front/src/constants/menu.items.constants.js
@@ -0,0 +1,18 @@
+export const MENU_ITEMS_LINKS = [
+  {
+    label: 'Home',
+    href: '/',
+  },
+  {
+    label: 'Quizz',
+    href: '/quizz',
+  },
+  {
+    label: 'About',
+    href: '/about',
+  },
+  {
+    label: 'Discount',
+    href: '/discount',
+  },
+];
