355f8d2c9c8bcea93ac752ceba88478731fdf729
Draxx0 <williamfort.lmgl@gmail.com>
2024-10-17 14:08:10 +0200

    :sparkles: add  button component

--- front/src/components/common/Button.jsx
+++ front/src/components/common/Button.jsx
@@ -1,16 +1,46 @@
+import { cva } from 'class-variance-authority';
+import { Link } from 'react-router-dom';
 import { cn } from '../../utils/utils';
 
-export const Button = ({ children, className, href }) => {
-  const classes = cn(
-    'bg-purple-600 text-white font-bold py-3 px-6 rounded-full hover:bg-purple-700 transition duration-300',
-    className
+export const Button = ({ children, className, href, variant = 'default' }) => {
+  const buttonVariants = cva(
+    'font-bold py-3 px-6 rounded-full hover:bg-purple-700 transition duration-300',
+    {
+      variants: {
+        variant: {
+          default: 'bg-purple-600 text-white',
+          outline:
+            'bg-transparent hover:bg-purple-100 text-purple-500 border border-purple-500 hover:border-purple-600',
+        },
+      },
+      defaultVariants: {
+        variant: 'default',
+      },
+    }
   );
 
   return href ? (
-    <a href={href} className={classes}>
+    <Link
+      to={href}
+      className={cn([
+        buttonVariants({
+          variant,
+          className,
+        }),
+      ])}
+    >
       {children}
-    </a>
+    </Link>
   ) : (
-    <button className={classes}>{children}</button>
+    <button
+      className={cn([
+        buttonVariants({
+          variant,
+          className,
+        }),
+      ])}
+    >
+      {children}
+    </button>
   );
 };
