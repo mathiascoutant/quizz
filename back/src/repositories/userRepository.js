d9fcf657f6a967fb7e5956f69ba67132f9ce7992
Jonathan Piboteau <jonathanpiboteau@gmail.com>
2024-11-07 12:14:13 +0100

    Add coins in user and returns coins

--- back/src/repositories/userRepository.js
+++ back/src/repositories/userRepository.js
@@ -11,5 +11,14 @@ export const UserRepository = {
     return await User.findByIdAndUpdate(id, updateData, { new: true }).select('-password');
   },
   deleteUser: async (id) => await User.findByIdAndDelete(id),
-  findUserCoinsById: async (id) => await UserCoins.findById(id)
+  findUserCoinsById: async (id) => await UserCoins.findById(id),
+  updateCoins: async (id, coinValue) => {
+    const user = await User.findByPk(id);
+    if (!user) throw new Error('User not found.');
+    const newCoinBalance = user.coins + coinValue;
+    console.log('newCoinBalance', newCoinBalance);
+    await User.update({ coins: newCoinBalance }, { where: { id } });
+
+    return newCoinBalance; 
+  }
 };
