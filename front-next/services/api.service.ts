131ca8f14fefcee1bc1f5e5c059796ee0aabb3ec
Draxx0 <williamfort.lmgl@gmail.com>
2024-11-25 13:21:03 +0100

    :sparkles: add api reusable function

--- front-next/services/api.service.ts
+++ front-next/services/api.service.ts
@@ -1,3 +1,19 @@
 export const constructUrl = (url: string) => {
   return `${process.env.NEXT_PUBLIC_API_URL}${url}`;
 };
+
+export const api = (
+  url: string,
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' = 'GET'
+) => {
+  const storage = JSON.parse(localStorage.getItem('session-storage') || '{}');
+  const token = storage.state.session.token;
+
+  return fetch(constructUrl(url), {
+    method,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
