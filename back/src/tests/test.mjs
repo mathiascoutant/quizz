a5bab3e104d632a635a6c2ba2f234626b1a2b40b
Mathias COUTANT <mathias.coutant@pxcom.aero>
2024-11-26 16:08:54 +0100

    Add tests

--- back/src/tests/test.mjs
+++ back/src/tests/test.mjs
@@ -1,90 +1,190 @@
 import supertest from 'supertest';
 import express from 'express';
-import { register, login } from '../controllers/authController'; // Assurez-vous que le chemin est correct
-import { getCategories } from '../controllers/categoryController'; // Assurez-vous que le chemin est correct
-import { getLevels } from '../controllers/listDifficultyController'; // Assurez-vous que le chemin est correct
-import { getForms } from '../controllers/formController'; // Assurez-vous que le chemin est correct
-import { getUserById, getUserProfile } from '../controllers/userController'; // Assurez-vous que le chemin est correct
-
+import { register, login } from '../controllers/authController';
+import { getCategories } from '../controllers/categoryController';
+import { getLevels } from '../controllers/coinController.js';
+import { getForms } from '../controllers/formController';
+import { getUserProfile } from '../controllers/userController';
+import { getTopUsersWithPosition } from '../controllers/userController.js';
+import { protect } from '../controllers/authController';
+import { getUserCouponsByUserId } from '../controllers/userCouponController.js';
+import { getAllCoupons } from '../controllers/couponController.js';
 const app = express();
 app.use(express.json());
-app.post('/auth/register', register); // Route pour l'inscription
-app.post('/auth/login', login); // Route pour la connexion
-app.get('/api/categories', getCategories); // Route pour récupérer les catégories
-app.get('/api/levels', getLevels); // Route pour récupérer les niveaux de difficulté
-app.get('/api/quizz', getForms); // Route pour récupérer toutes les questions
-app.get('/profile', getUserProfile); // Route pour récupérer le profil
+app.post('/api/auth/register', register);
+app.post('/api/auth/login', login);
+app.get('/api/categories', getCategories);
+app.get('/api/levels', getLevels);
+app.get('/api/quizz', getForms);
+app.get('/api/profile', getUserProfile);
+app.get('/api/classement/top-users', protect, getTopUsersWithPosition);
+app.get('/api/user-coupons/:userId', getUserCouponsByUserId);
+app.get('/api/coupons', getAllCoupons);
 
 describe('Tests pour l\'inscription et la connexion', () => {
-    
+    const userCredentials = {
+        email: 'hehe5@example.com',
+        pseudo: 'hehe5'
+    };
+
     test('Devrait inscrire un nouvel utilisateur', async () => {
         const newUser = {
             firstname: 'mma',
             lastname: 'mma',
-            pseudo: 'mmaaa',
-            email: 'jmmaaa@example.com',
+            pseudo: userCredentials.pseudo,
+            email: userCredentials.email,
             password: 'passssword123'
         };
 
-        const response = await supertest(app).post('/auth/register').send(newUser);
-        
-        expect(response.status).toBe(201); // Vérifiez que l'inscription a réussi
+        const response = await supertest(app).post('/api/auth/register').send(newUser);
+        expect(response.status).toBe(201);
         expect(response.body).toHaveProperty('message', 'Utilisateur enregistré avec succès');
     });
 
     test('Devrait connecter un utilisateur existant', async () => {
-        const userCredentials = {
-            email: 'jmmaa@example.com', // Assurez-vous que cet utilisateur existe
-            password: 'passssword123' // Assurez-vous que le mot de passe est correct
+        const userLoginCredentials = {
+            email: userCredentials.email,
+            password: 'passssword123'
         };
 
-        const response = await supertest(app).post('/auth/login').send(userCredentials);
-        
-        expect(response.status).toBe(200); // Vérifiez que la connexion a réussi
-        expect(response.body).toHaveProperty('token'); // Vérifiez que le token est renvoyé
+        const response = await supertest(app).post('/api/auth/login').send(userLoginCredentials);
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('token');
     });
 
     test('Devrait récupérer les catégories', async () => {
         const response = await supertest(app).get('/api/categories');
-        
-        expect(response.status).toBe(200); // Vérifiez que la récupération a réussi
-        expect(response.body).toBeInstanceOf(Array); // Vérifiez que la réponse est un tableau
-        expect(response.body.length).toBeGreaterThan(0); // Vérifiez qu'il y a au moins une catégorie
+        expect(response.status).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body.length).toBeGreaterThan(0);
     });
 
     test('Devrait récupérer les niveaux de difficulté', async () => {
         const response = await supertest(app).get('/api/levels');
-        
-        expect(response.status).toBe(200); // Vérifiez que la récupération a réussi
-        expect(response.body).toBeInstanceOf(Array); // Vérifiez que la réponse est un tableau
-        expect(response.body.length).toBeGreaterThan(0); // Vérifiez qu'il y a au moins un niveau de difficulté
+        expect(response.status).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body.length).toBeGreaterThan(0);
     });
 
     test('Devrait récupérer toutes les questions', async () => {
         const response = await supertest(app).get('/api/quizz');
-        
-        expect(response.status).toBe(200); // Vérifiez que la récupération a réussi
-        expect(response.body).toBeInstanceOf(Array); // Vérifiez que la réponse est un tableau
-        expect(response.body.length).toBeGreaterThan(0); // Vérifiez qu'il y a au moins une question
+        expect(response.status).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body.length).toBeGreaterThan(0);
     });
 
     test('Devrait récupérer le profil', async () => {
-        // Pour ce test, vous devez d'abord vous connecter pour obtenir un token
-        const userCredentials = {
-            email: 'jmmaa@example.com', // Assurez-vous que cet utilisateur existe
-            password: 'passssword123' // Assurez-vous que le mot de passe est correct
+        const userLoginCredentials = {
+            email: userCredentials.email,
+            password: 'passssword123'
         };
 
-        const loginResponse = await supertest(app).post('/auth/login').send(userCredentials);
-        const token = loginResponse.body.token; // Récupérez le token
+        const loginResponse = await supertest(app).post('/api/auth/login').send(userLoginCredentials);
+        const token = loginResponse.body.token;
 
         const response = await supertest(app)
-            .get('/profile')
-            .send({ token }); // Envoyez le token dans le corps de la requête
+            .get('/api/profile')
+            .set('Authorization', `Bearer ${token}`);
 
-        expect(response.status).toBe(200); // Vérifiez que la récupération a réussi
-        expect(response.body).toHaveProperty('email', 'jmmaa@example.com'); // Vérifiez que l'email est correct
-        expect(response.body).toHaveProperty('firstname'); // Vérifiez que le prénom est présent
-        expect(response.body).toHaveProperty('lastname'); // Vérifiez que le nom est présent
+        console.log(response.body);
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('user');
+        expect(response.body.user).toHaveProperty('email', userCredentials.email);
+        expect(response.body.user).toHaveProperty('badges');
+        expect(response.body.user.badges).toBeInstanceOf(Array);
+    });
+});
+
+describe('Tests pour le classement des utilisateurs', () => {
+    test('Devrait récupérer les 3 meilleurs utilisateurs et la position de l\'utilisateur courant', async () => {
+        const userLoginCredentials = {
+            email: 'hehe4@example.com',
+            password: 'passssword123'
+        };
+
+        const loginResponse = await supertest(app).post('/api/auth/login').send(userLoginCredentials);
+        const token = loginResponse.body.token;
+
+        const response = await supertest(app)
+            .get('/api/classement/top-users')
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty('topUsers');
+        expect(response.body.topUsers).toBeInstanceOf(Array);
+        expect(response.body.topUsers.length).toBe(3);
+
+        response.body.topUsers.forEach(user => {
+            expect(user).toHaveProperty('id');
+            expect(user).toHaveProperty('pseudo');
+            expect(user).toHaveProperty('coins');
+        });
+
+        expect(response.body).toHaveProperty('currentUser');
+        expect(response.body.currentUser).toHaveProperty('id');
+        expect(response.body.currentUser).toHaveProperty('pseudo');
+        expect(response.body.currentUser).toHaveProperty('coins');
+        expect(response.body.currentUser).toHaveProperty('position');
+    });
+});
+
+describe('Tests pour la récupération des coupons de l\'utilisateur', () => {
+    test('Devrait récupérer les coupons de l\'utilisateur', async () => {
+        const userId = 41;
+        const userLoginCredentials = {
+            email: 'hehe4@example.com',
+            password: 'passssword123'
+        };
+
+        const loginResponse = await supertest(app).post('/api/auth/login').send(userLoginCredentials);
+        const token = loginResponse.body.token;
+
+        const response = await supertest(app)
+            .get(`/api/user-coupons/${userId}`)
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body.length).toBeGreaterThan(0);
+
+        response.body.forEach(coupon => {
+            expect(coupon).toHaveProperty('id');
+            expect(coupon).toHaveProperty('userId', userId);
+            expect(coupon).toHaveProperty('couponId');
+            expect(coupon).toHaveProperty('discountCode');
+            expect(coupon).toHaveProperty('coupon');
+        });
+    });
+});
+
+describe('Tests pour la récupération de tous les coupons', () => {
+    test('Devrait récupérer tous les coupons', async () => {
+        const userLoginCredentials = {
+            email: 'hehe4@example.com',
+            password: 'passssword123'
+        };
+
+        const loginResponse = await supertest(app).post('/api/auth/login').send(userLoginCredentials);
+        const token = loginResponse.body.token;
+
+        const response = await supertest(app)
+            .get('/api/coupons')
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body.length).toBeGreaterThan(0);
+
+        response.body.forEach(coupon => {
+            expect(coupon).toHaveProperty('id');
+            expect(coupon).toHaveProperty('cashReduction');
+            expect(coupon).toHaveProperty('percentReduction');
+            expect(coupon).toHaveProperty('nameNominator');
+            expect(coupon).toHaveProperty('brand');
+            expect(coupon).toHaveProperty('specificContent');
+            expect(coupon).toHaveProperty('coinCost');
+            expect(coupon).toHaveProperty('validityDate');
+            expect(coupon).toHaveProperty('color');
+        });
     });
 });
