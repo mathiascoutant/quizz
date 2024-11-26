5b6a78028c8d3313f6695c8fc4ac837fb26d337a
idryssj31 <55395114+idryssj31@users.noreply.github.com>
2024-11-26 14:31:25 +0100

    [UPD] : ranking page

---
+++ back/src/routes/classementRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import * as userController from "../controllers/userController.js";
+import { protect } from "../controllers/authController.js";
+const router = express.Router();
+
+// Route pour l'inscription
+router.get("/", userController.getUserProfile);
+router.put("/update/:userId", userController.updateUser);
+router.get("/coins", userController.getUserCoins);
+
+router.get("/some-route", (req, res) => {
+  // Ajoutez ici la logique de votre route
+  res.send("Réponse de la route GET");
+});
+
+export default router;
