477905c75903d39f7852147ea5f5eef794a3871e
idryssj31 <55395114+idryssj31@users.noreply.github.com>
2024-11-28 11:29:49 +0100

    [ADD] : color of the button and register

--- front/containers/profile/ProfileTab.tsx
+++ front/containers/profile/ProfileTab.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@/components/common/Button';
-import { api } from '@/services/api.service';
-import { User, useSessionStore } from '@/store/session.store';
-import { useEffect, useState, useTransition } from 'react';
-import { toast } from 'sonner';
+import { Button } from "@/components/common/Button";
+import { api } from "@/services/api.service";
+import { User, useSessionStore } from "@/store/session.store";
+import { useEffect, useState, useTransition } from "react";
+import { toast } from "sonner";
 
 export const ProfileTab = () => {
   const { session, updateUser } = useSessionStore();
@@ -15,22 +15,34 @@ export const ProfileTab = () => {
     email: null,
   });
 
+  // État pour stocker les données initiales
+  const [initialData, setInitialData] = useState<Record<string, string | null>>(
+    {
+      firstname: null,
+      lastname: null,
+      pseudo: null,
+      email: null,
+    }
+  );
+
   useEffect(() => {
-    if (session) {
-      setNewData({
+    if (session && session.user) {
+      const userData = {
         firstname: session.user.firstname,
         lastname: session.user.lastname,
         pseudo: session.user.pseudo,
         email: session.user.email,
-      });
+      };
+      setNewData(userData);
+      setInitialData(userData);
     }
   }, [session]);
 
   const fieledLabels = {
-    firstname: 'Prénom',
-    lastname: 'Nom',
-    pseudo: 'Pseudo',
-    email: 'Email',
+    firstname: "Prénom",
+    lastname: "Nom",
+    pseudo: "Pseudo",
+    email: "Email",
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,9 +50,17 @@ export const ProfileTab = () => {
     setNewData((prevState) => ({ ...prevState, [name]: value }));
   };
 
+  // Fonction pour vérifier si les données ont été modifiées
+  const isUpdated = () => {
+    return JSON.stringify(newData) !== JSON.stringify(initialData);
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    if (!session) return;
+    if (!session || !session.user) {
+      toast.error("La session utilisateur est introuvable.");
+      return;
+    }
 
     try {
       const { ...payload } = newData;
@@ -48,7 +68,7 @@ export const ProfileTab = () => {
       startTransition(async () => {
         const response = await api(
           `/profile/update/${session.user.id}`,
-          'PUT',
+          "PUT",
           payload
         );
 
@@ -64,10 +84,10 @@ export const ProfileTab = () => {
         toast.success(message);
       });
     } catch (error) {
-      toast.error('Une erreur est survenue lors de la mise à jour du profil');
+      toast.error("Une erreur est survenue lors de la mise à jour du profil");
       if (error instanceof Error) {
         throw new Error(
-          'Une erreur est survenue lors de la mise à jour du profil'
+          "Une erreur est survenue lors de la mise à jour du profil"
         );
       }
     }
@@ -80,8 +100,8 @@ export const ProfileTab = () => {
         <input
           id="input-form"
           name={name}
-          type={'text'}
-          defaultValue={newData[name] || ''}
+          type={"text"}
+          defaultValue={newData[name] || ""}
           onChange={handleChange}
           className="border border-gray-300 rounded p-2"
         />
@@ -93,8 +113,19 @@ export const ProfileTab = () => {
     <div className="bg-white rounded-lg shadow-md p-8">
       <h1 className="text-4xl font-bold mb-4">Mon compte</h1>
       <form onSubmit={handleSubmit} className="space-y-6">
-        {Object.keys(newData).map((name, index) => renderField(name, index))}
-        <Button isLoading={pending} type="submit" className="w-full">
+        {Object.keys(newData).map((name, index) =>
+          renderField(name as keyof typeof fieledLabels, index)
+        )}
+        <Button
+          isLoading={pending}
+          type="submit"
+          className={`w-full opacity-50 ${
+            isUpdated()
+              ? "bg-purple-500 hover:bg-purple-700 opacity-100 text-white"
+              : "bg-gray-300 text-gray-600"
+          }`}
+          disabled={!isUpdated()} // Désactive le bouton si aucune modification n'a été faite
+        >
           Mettre à jour
         </Button>
       </form>
